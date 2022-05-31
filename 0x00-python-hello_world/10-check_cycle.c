be2f2a4a43636c8941832e020340a345c1dfba85
Godwin Martins <gmcodes@gmail.com>
2022-05-31 05:59:42 +0100

    create  10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a list has a cycle
+ * @list: The list to be checked
+ *
+ * Return: 0 if there is no cycle, otherwise 1
+ */
+
+
+
+int check_cycle(listint_t *list)
+{
+	int cycle = 0;
+
+	if (!list)
+		return (cycle);
+/*
+ *	myList = list, my_cycle = myList->next;
+*/
+	return (cycle);
+}
