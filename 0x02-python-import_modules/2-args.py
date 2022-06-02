6541b0e942e7684a25f4785538afd857515385dd
Godwin Martins <gmcodes@gmail.com>
2022-06-02 21:20:30 +0100

    modify 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    n = len(sys.argv)
+    from sys import argv
+    n = len(argv)
     if n <= 1:
-        print("{:d} arguements.".format(n-1))
+        print("{:d} arguments.".format(n-1))
 
     elif n == 2:
-        print("{:d} arguement:".format(n-1))
+        print("{:d} argument:".format(n-1))
         for i in range(1, n):
-            print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d}: {}".format(i, argv[i]))
     else:
-        print("{:d} arguements:".format(n-1))
+        print("{:d} arguments:".format(n-1))
         for i in range(1, n):
-            print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d}: {}".format(i, argv[i]))
