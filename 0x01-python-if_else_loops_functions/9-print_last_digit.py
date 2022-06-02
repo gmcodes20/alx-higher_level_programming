7952062b4c9711b189c19f7d3b4ffffa1b3b3484
Godwin Martins <gmcodes@gmail.com>
2022-06-02 04:09:06 +0100

    modify 8-uppercase.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    my_number = number % 10
-    print("{:1}".format(my_number), end = "")
+    my_number = abs(number % 10)
+    print("{:1}".format(my_number), end="")
     return my_number
