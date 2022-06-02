e299d1d9b027a9c04064763a6f9967d38fc09e80
Godwin Martins <gmcodes@gmail.com>
2022-06-02 03:05:33 +0100

    Modify  7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in (range(ord('a') and ord('Z'))):
-        if ord(c) > 64 and ord(c) < 91:
+        if ord(c) > 97 and ord(c) < 123:
             return True
         else:
             return False
