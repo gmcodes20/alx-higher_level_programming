81b396cb385940de7efe5bd7d644aee9081edf12
Godwin Martins <gmcodes@gmail.com>
2022-06-04 23:10:25 +0100

    Modify 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     if my_list:
         list_len = len(my_list)
         for i in range(list_len):
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
