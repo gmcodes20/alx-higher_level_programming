46da239523c815b23130ed35f5c9d8a2ce46dfa7
Godwin Martins <gmcodes@gmail.com>
2022-06-06 15:00:55 +0100

    Create  4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,8 @@
 def new_in_list(my_list, idx, element):
     my_own_list = my_list[:]
     list_lenght = len(my_list)
-    if idx < 0:
+    if idx < 0 or idx >= list_lenght:
         return my_list
-    if idx > list_lenght:
-        return my_list
-    for val in range(list_lenght):
-        if my_list.index(my_list[val]) == idx:
-            my_own_list[val] = element
-            return my_own_list
+    else:
+        my_own_list[idx] = element
+        return my_own_list
