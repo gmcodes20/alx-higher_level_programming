4ec54821b6efdd5bcbb1a91af795f080c9b5bfd9
Godwin Martins <gmcodes@gmail.com>
2022-06-06 14:54:02 +0100

    modify 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     list_lenght = len(my_list)
-    if idx < 0 or idx > list_lenght:
+    if idx < 0 or idx >= list_lenght:
         return my_list
     else:
         my_list[idx] = element
