c84739119dea1b6896263cee9cf1ab08d13c1200
Godwin Martins <gmcodes@gmail.com>
2022-06-06 14:46:31 +0100

    modify 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,11 +2,10 @@
 
 def replace_in_list(my_list, idx, element):
     list_lenght = len(my_list)
-    if idx < 0:
+    if idx < 0 or idx > list_lenght:
         return my_list
-    if idx > list_lenght:
-        return my_list
-    for val in range(list_lenght):
-        if my_list.index(my_list[val]) == idx:
+    else:
+        for val in range(list_lenght):
+            if my_list.index(my_list[val]) == idx:
             my_list[val] = element
             return my_list
